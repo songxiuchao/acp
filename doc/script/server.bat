d2dc3a7f96a3355e2b5e40bbec3cb23e5e001a91
zhangbin1010 <391289241@qq.com>
2018-09-27 15:19:19 +0800

    修改jvm参数

--- doc/script/server.bat
+++ doc/script/server.bat
@@ -1,2 +1,2 @@
-java -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=0 -Xms1024m -Xmx4086m -Dfile.encoding=utf-8 -jar xxx.jar
+java -server -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -Xlog:age*,gc*=info:file=gc-%t.log:time,tid,tags:filecount=3,filesize=20m -Djava.io.tmpdir=jvmlogs -Xms1024m -Xmx4086m -Dfile.encoding=utf-8 -jar xxx.jar
 echo. & pause
