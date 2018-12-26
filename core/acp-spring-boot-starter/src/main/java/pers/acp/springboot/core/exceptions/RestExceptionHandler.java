e7685d8fc779b8406d9d5d4757db80ac5092ddac
zhangbin <zb881010>
2018-12-26 20:20:49 +0800

    优化全局异常处理

--- core/acp-spring-boot-starter/src/main/java/pers/acp/springboot/core/exceptions/RestExceptionHandler.java
+++ core/acp-spring-boot-starter/src/main/java/pers/acp/springboot/core/exceptions/RestExceptionHandler.java
@@ -68,7 +68,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         } else {
             json = PackageTools.buildErrorResponsePackage(ResponseCode.otherError, ex.getMessage());
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).header("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE).body(json.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).header("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE).body(json.toString());
     }
 
     /**
@@ -83,8 +83,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         log.error(ex.getMessage(), ex);
-        headers.set("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).body(PackageTools.buildErrorResponsePackage(ResponseCode.invalidParameter, ex.getMessage()).toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).header("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE).body(PackageTools.buildErrorResponsePackage(ResponseCode.invalidParameter, ex.getMessage()).toString());
     }
 
     /**
@@ -99,8 +98,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         log.error(ex.getMessage(), ex);
-        headers.set("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).body(PackageTools.buildErrorResponsePackage(ResponseCode.invalidParameter, ex.getMessage()).toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).header("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE).body(PackageTools.buildErrorResponsePackage(ResponseCode.invalidParameter, ex.getMessage()).toString());
     }
 
 }
