d1f840ae65bc7a129c9e96a66c4b5b2be0e0e50a
zhangbin <zb881010>
2019-03-19 22:11:13 +0800

    [Upgrade] 优化代码

--- test/testspringboot/src/main/java/pers/acp/test/application/conf/BeansDefine.java
+++ test/testspringboot/src/main/java/pers/acp/test/application/conf/BeansDefine.java
@@ -1,15 +1,15 @@
-package pers.acp.test.application.component;
+package pers.acp.test.application.conf;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateExceptionTranslator;
-import org.springframework.stereotype.Component;
 
 /**
  * @author zhangbin by 2018-3-2 0:41
  * @since JDK 11
  */
-@Component
+@Configuration
 public class BeansDefine {
 
     @Bean
