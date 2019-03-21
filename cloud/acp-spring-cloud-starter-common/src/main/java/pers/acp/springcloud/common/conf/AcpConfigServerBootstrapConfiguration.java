d3d15d208419aac9b3f089d5a96c8c5115ba6d9f
zhangbin <zb881010>
2019-03-21 14:48:30 +0800

    [Upgrade] 优化自定义 PropertySourceLocator 配置

--- cloud/acp-spring-cloud-starter-common/src/main/java/pers/acp/springcloud/common/conf/AcpConfigServerBootstrapConfiguration.java
+++ cloud/acp-spring-cloud-starter-common/src/main/java/pers/acp/springcloud/common/conf/AcpConfigServerBootstrapConfiguration.java
@@ -1,7 +1,13 @@
-package pers.acp.springcloud.common.component;
+package pers.acp.springcloud.common.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.client.ConfigClientProperties;
 import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -18,20 +24,29 @@ import java.util.Map;
 import static org.springframework.cloud.config.client.ConfigClientProperties.AUTHORIZATION;
 
 /**
- * @author zhang by 28/02/2019
+ * @author zhang by 21/03/2019
  * @since JDK 11
  */
-public class CustomerConfigServicePropertySourceLocator extends ConfigServicePropertySourceLocator {
+@Configuration
+@EnableConfigurationProperties
+public class AcpConfigServerBootstrapConfiguration {
 
     private final LogFactory log = LogFactory.getInstance(this.getClass());
 
     private final ConfigurableEnvironment environment;
 
-    public CustomerConfigServicePropertySourceLocator(ConfigurableEnvironment environment, ConfigClientProperties clientProperties) {
-        super(clientProperties);
+    @Autowired
+    public AcpConfigServerBootstrapConfiguration(ConfigurableEnvironment environment) {
         this.environment = environment;
-        setRestTemplate(customerConfigClientRestTemplate(clientProperties));
-        log.info("Start Up Cloud, Configuration CustomerConfigServicePropertySourceLocator For ACP");
+    }
+
+    @Primary
+    @Bean
+    @ConditionalOnProperty(value = "spring.cloud.config.enabled", matchIfMissing = true)
+    public ConfigServicePropertySourceLocator configServicePropertySource(ConfigClientProperties clientProperties) {
+        ConfigServicePropertySourceLocator locator = new ConfigServicePropertySourceLocator(clientProperties);
+        locator.setRestTemplate(customerConfigClientRestTemplate(clientProperties));
+        return locator;
     }
 
     private RestTemplate customerConfigClientRestTemplate(ConfigClientProperties clientProperties) {
