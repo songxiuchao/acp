3c424f9e16a681a5825455e5ecbe9c4e074a5dab
zhangbin <zb881010>
2019-05-16 01:16:36 +0800

    [Upgrade] 优化线程池参数设定及注释

--- core/acp-core/src/main/java/pers/acp/core/task/threadpool/ThreadPoolService.java
+++ core/acp-core/src/main/java/pers/acp/core/task/threadpool/ThreadPoolService.java
@@ -25,36 +25,39 @@ public final class ThreadPoolService {
     /**
      * 获取线程池实例
      *
+     * @param minThreadNumber 最小线程数
      * @param maxThreadNumber 最大线程数
      * @param querySize       队列容量
      * @return 线程池实例
      */
-    public static ThreadPoolService getInstance(int maxThreadNumber, int querySize) {
-        return getInstance(60000, maxThreadNumber, querySize);
+    public static ThreadPoolService getInstance(int minThreadNumber, int maxThreadNumber, int querySize) {
+        return getInstance(60000, minThreadNumber, maxThreadNumber, querySize);
     }
 
     /**
      * 获取线程池实例
      *
      * @param maxFreeTime     线程最大空闲时间，单位毫秒
+     * @param minThreadNumber 最小线程数
      * @param maxThreadNumber 最大线程数
      * @param querySize       队列容量
      * @return 线程池实例
      */
-    public static ThreadPoolService getInstance(long maxFreeTime, int maxThreadNumber, int querySize) {
-        return getInstance(null, maxFreeTime, maxThreadNumber, querySize);
+    public static ThreadPoolService getInstance(long maxFreeTime, int minThreadNumber, int maxThreadNumber, int querySize) {
+        return getInstance(null, maxFreeTime, minThreadNumber, maxThreadNumber, querySize);
     }
 
     /**
      * 获取线程池实例
      *
      * @param poolName        线程池实例名
+     * @param minThreadNumber 最小线程数
      * @param maxThreadNumber 最大线程数
      * @param querySize       队列容量
      * @return 线程池实例
      */
-    public static ThreadPoolService getInstance(String poolName, int maxThreadNumber, int querySize) {
-        return getInstance(poolName, 60000, maxThreadNumber, querySize);
+    public static ThreadPoolService getInstance(String poolName, int minThreadNumber, int maxThreadNumber, int querySize) {
+        return getInstance(poolName, 60000, minThreadNumber, maxThreadNumber, querySize);
     }
 
     /**
@@ -62,19 +65,20 @@ public final class ThreadPoolService {
      *
      * @param poolName        线程池实例名
      * @param maxFreeTime     线程最大空闲时间
+     * @param minThreadNumber 最小线程数
      * @param maxThreadNumber 最大线程数
      * @param querySize       队列容量
      * @return 线程池实例
      */
-    public static ThreadPoolService getInstance(String poolName, long maxFreeTime, int maxThreadNumber, int querySize) {
+    public static ThreadPoolService getInstance(String poolName, long maxFreeTime, int minThreadNumber, int maxThreadNumber, int querySize) {
         ThreadPoolService instance;
         if (CommonUtils.isNullStr(poolName)) {
             poolName = "defaultThreadPool";
         }
         synchronized (ThreadPoolService.class) {
             if (!threadPoolInstanceMap.containsKey(poolName)) {
-                instance = new ThreadPoolService(poolName, maxFreeTime, maxThreadNumber, querySize);
-                log.debug("init ThreadPool [" + poolName + "] success, maxThread:" + maxThreadNumber);
+                instance = new ThreadPoolService(poolName, maxFreeTime, minThreadNumber, maxThreadNumber, querySize);
+                log.debug("init ThreadPool [" + poolName + "] success, minThread:" + minThreadNumber + " maxThread:" + maxThreadNumber);
                 threadPoolInstanceMap.put(poolName, instance);
             } else {
                 instance = threadPoolInstanceMap.get(poolName);
@@ -88,20 +92,51 @@ public final class ThreadPoolService {
      *
      * @param poolName        线程池实例名
      * @param maxFreeTime     线程最大空闲时间
+     * @param minThreadNumber 最小线程数
      * @param maxThreadNumber 最大线程数
      *                        Integer.MAX_VALUE--无界线程数，直接提交
      * @param querySize       队列容量；
      *                        Integer.MAX_VALUE--无界队列，LinkedBlockingQueue
      *                        else---------------有界队列，ArrayBlockingQueue
      */
-    private ThreadPoolService(String poolName, long maxFreeTime, int maxThreadNumber, int querySize) {
+    private ThreadPoolService(String poolName, long maxFreeTime, int minThreadNumber, int maxThreadNumber, int querySize) {
         this.poolName = poolName;
-        if (maxThreadNumber == Integer.MAX_VALUE) {// 直接提交
+        if (maxThreadNumber == Integer.MAX_VALUE) {
+            /*
+             * 直接提交队列: SynchronousQueue
+             * 1、要求无界 maximumPoolSizes 以避免拒绝新提交的任务。其中每个插入操作必须等待另一个线程的对应移除操作，也就是说A任务进入队列，B任务必须等A任务被移除之后才能进入队列，否则执行异常策略
+             * 2、运行线程数 <  corePoolSize 时直接创建线程运行
+             * 3、运行线程数 >= corePoolSize 任务放入队列
+             * 4、队列中的任务处理逻辑：
+             *    运行线程数 < maximumPoolSizes 时创建新线程运行待加入队列的任务
+             *    运行线程数 = maximumPoolSizes 时执行异常策略
+             * 5、当一个线程空闲时间超过 maxFreeTime 且运行线程数 > corePoolSize 时，销毁空闲线程，直至运行线程数 = corePoolSize
+             */
             executor = new ThreadPoolExecutor(0, maxThreadNumber, maxFreeTime, TimeUnit.MILLISECONDS, new SynchronousQueue<>());
-        } else if (querySize == Integer.MAX_VALUE) {// 无界队列
-            executor = new ThreadPoolExecutor(0, maxThreadNumber, maxFreeTime, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
-        } else {// 有界队列
-            executor = new ThreadPoolExecutor(0, maxThreadNumber, maxFreeTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(querySize));
+        } else if (querySize == Integer.MAX_VALUE) {
+            /*
+             * 无界队列: LinkedBlockingQueue
+             * 1、将导致在所有核心线程都在忙时新任务在队列中等待。这样，创建的线程就不会超过 corePoolSize。(因此，maximumPoolSize 的值也就没意义了。)
+             * 2、运行线程数 <  corePoolSize 时直接创建线程运行
+             * 3、运行线程数 >= corePoolSize 任务放入队列，
+             * 4、队列中的任务处理逻辑：
+             *    在队列中一直排队等待，直至有空闲线程继续处理或内存溢出
+             */
+            executor = new ThreadPoolExecutor(maxThreadNumber, maxThreadNumber, 0, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+        } else {
+            /*
+             * 有界队列: ArrayBlockingQueue
+             * 1、正常线程池，需适当调整 corePoolSize、maximumPoolSizes、querySize 的参数
+             * 2、运行线程数 <  corePoolSize 时直接创建线程运行
+             * 3、运行线程数 >= corePoolSize 任务放入队列
+             * 4、队列中的任务处理逻辑：
+             *    运行线程数 < maximumPoolSizes 且队列未满时（新任务加入队列前），新任务加入队列中一直排队等待
+             *    运行线程数 < maximumPoolSizes 且队列已满时（新任务加入队列前），创建新线程运行待加入队列的任务
+             *    运行线程数 = maximumPoolSizes 且队列未满时（新任务加入队列前），新任务加入队列中一直排队等待
+             *    运行线程数 = maximumPoolSizes 且队列已满时（新任务加入队列前），执行异常策略
+             * 5、当一个线程空闲时间超过 maxFreeTime 且运行线程数 > corePoolSize 时，销毁空闲线程，直至运行线程数 = corePoolSize
+             */
+            executor = new ThreadPoolExecutor(minThreadNumber, maxThreadNumber, maxFreeTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(querySize));
         }
     }
 
