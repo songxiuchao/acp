f9b891f7fdeda83c111d365725d257c54ba931d4
zhangbin1010 <391289241@qq.com>
2019-12-25 20:59:10 +0800

    [Upgrade] 增加测试方法

--- test/testkotlin/src/test/kotlin/pers/acp/kotlin/test/Test.kt
+++ test/testkotlin/src/test/kotlin/pers/acp/kotlin/test/Test.kt
@@ -38,7 +38,22 @@ fun main(args: Array<String>) {
     println(file.canonicalPath)
     println(file.length())
 
+    testSpEL()
+}
+
+/**
+ * 测试 SpringEL 表达式
+ */
+fun testSpEL() {
+    val path = "C:\\WorkFile\\IdeaProjects\\pers-acp\\acp-admin-cloud\\logs\\log-server"
+    val fileName = "..\\..\\log-server.log"
+    val file = File("$path\\$fileName")
     val parser = SpelExpressionParser()
-    val exp = parser.parseExpression("canonicalPath")
-    println("Expression Value: ${exp.getValue(file)}")
+    val exp1 = parser.parseExpression("canonicalPath")
+    println("Expression1 Value: ${exp1.getValue(file)}")
+
+    val testData = TestData()
+    val exp2 = parser.parseExpression("test2=13")
+    println("Expression2 Value: ${exp2.getValue(testData)}")
+    println(testData)
 }
